fd22db6056f97518451caafdffa3666850350366
QPCrummer <qpcrummer@gmail.com>
2025-02-22 00:09:53 -0500

    More MathHelper functions

--- src/main/rust/util/math_helper.rs
+++ src/main/rust/util/math_helper.rs
@@ -3,6 +3,9 @@ use ape_table_trig::*;
 const APPROXIMATION_THRESHOLD: f32 = 1.0E-5;
 const PACKED_DEGREES_CONSTANT: f32 = 360.0 / 256.0;
 const UNPACKED_DEGREES_CONSTANT: f32 = 256.0 / 360.0;
+const MULTIPLY_DE_BRUIJN_BIT_POS: [i32; 32] = [
+    0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+];
 
 static TABLE: [f32; 65536] = trig_table_gen_f32!(65536);
 
@@ -191,8 +194,136 @@ pub fn wrap_degrees_double(degrees: f64) -> f64 {
     ((degrees % 360.0) + 540.0) % 360.0
 }
 
-// STOPPED AT LINE 298 IN MATH_HELPER CLASS
+/// Subtracts end from start
+pub fn subtract_angles(start: f32, end: f32) -> f32 {
+    wrap_degrees_float(end - start)
+}
 
+/// Absolute value of second - first
+pub fn angle_between (first: f32, second: f32) -> f32 {
+    subtract_angles(first, second).abs()
+}
+
+/// Clamps mean - value between the delta
+pub fn clamp_angle(value: f32, mean: f32, delta: f32) -> f32 {
+    let f: f32 = subtract_angles(value, mean);
+    mean - clamp_float(f, -delta, delta)
+}
+
+/// Steps from towards to, only changing the amount by at most step
+pub fn step_towards(from: f32, to: f32, mut step: f32) -> f32 {
+    step = step.abs();
+    if from < to {
+        clamp_float(from + step, from, to)
+    } else {
+        clamp_float(from - step, to, from)
+    }
+}
+
+/// Steps from towards to, only changing the amount by at most step
+pub fn step_unwrapped_angle_towards(from: f32, to: f32, step: f32) -> f32 {
+    step_towards(from, from + subtract_angles(from, to), step)
+}
+
+/// Parses the int.
+/// If the parse fails, it reverts to fallback
+pub fn parse_int(string: String, fallback: i32) -> i32 {
+    i32::from_str(&string).unwrap_or(fallback)
+}
+
+/// Gets the smallest power of two
+pub fn smallest_encompassing_power_of_two(value: i32) -> i32 {
+    let mut i = value - 1;
+    i |= i >> 1;
+    i |= i >> 2;
+    i |= i >> 4;
+    i |= i >> 8;
+    i |= i >> 16;
+    i + 1
+}
+
+/// Returns if the value is a power of two
+pub fn is_power_of_two(value: i32) -> bool {
+    value != 0 && (value & (value - 1)) == 0
+}
+
+/// Takes the ceiling of the log2(value) using the de Bruijn sequence
+pub fn ceil_log_2(mut value: i32) -> i32 {
+    value = if is_power_of_two(value) {
+        value
+    } else {
+        smallest_encompassing_power_of_two(value)
+    };
+    MULTIPLY_DE_BRUIJN_BIT_POS[(value as i64 * 125613361 >> 27) as i32 & 31]
+}
+
+/// Takes the floor of log2(value) using the de Bruijn sequence
+pub fn floor_log_2(value: i32) -> i32 {
+    ceil_log_2(value) - if is_power_of_two(value) {0} else {1}
+}
+
+/// Gets the fractional part of a float
+pub fn fractional_part_float(value: f32) -> f32 {
+    value.fract()
+}
+
+/// Gets the fractional part of a float
+pub fn fractional_part_double(value: f64) -> f64 {
+    value.fract()
+}
+
+/// Generates a hash code
+pub fn hash_code(x: i32, y: i32, z: i32) -> i64 {
+    let mut l: i64 = (x * 3129871) as i64 ^ (z as i64 * 116129781) ^ y as i64;
+    l = l * l * 42317861 + l * 11 as i64;
+    l >> 16
+}
+
+// TODO Generate random UUID
+
+/// Gets the fraction of the way value is between start and end
+pub fn get_lerp_progress_double(value: f64, start: f64, end: f64) -> f64 {
+    (value - start) / (end - start)
+}
+
+/// Gets the fraction of the way value is between start and end
+pub fn get_lerp_progress_float(value: f32, start: f32, end: f32) -> f32 {
+    (value - start) / (end - start)
+}
+
+// TODO Implement method_34945 with Vec3d
+
+/// Approximates the atan2 function
+pub fn atan_2(mut y: f64, mut x: f64) -> f64 {
+    let d: f64 = x * x + y * y;
+    if d.is_nan() {
+        f64::NAN
+    } else {
+        let bl: bool = y < 0.0;
+        let bl2: bool = x < 0.0;
+
+        if bl {
+            y = -y;
+        }
+
+        if bl2 {
+            x = -x;
+        }
+
+        let bl3: bool = y > x;
+        if bl3 {
+            let e: f64 = x;
+            x = y;
+            y = e;
+        }
+
+        // TODO Fast inverse square
+    }
+}
+
+pub fn inverse_sqrt(x: f32) {
+    
+}
 
 
 /// Linear Interpolation from start to end over delta time
