d3853466dcdeff384440033054a8318eb0933637
d-catte <qpcrummer@gmail.com>
2025-06-08 02:58:16 -0400

    Further reduce memory usage (Java)

--- src/main/java/com/github/tatercertified/oxidizium/utils/Cleanup.java
+++ src/main/java/com/github/tatercertified/oxidizium/utils/Cleanup.java
@@ -2,6 +2,7 @@ package com.github.tatercertified.oxidizium.utils;
 
 import com.github.tatercertified.oxidizium.Config;
 import net.minecraft.util.math.MathHelper;
+import org.joml.Vector3f;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
@@ -15,16 +16,34 @@ public final class Cleanup {
 
     private static void cleanupMathHelper() {
         try {
-            removeArray(MathHelper.class, "field_15727", double[].class);
-            removeArray(MathHelper.class, "field_15722", double[].class);
-            removeArray(MathHelper.class, "field_15725", float[].class);
+            removeField(MathHelper.class, "field_15727", double[].class);
+            removeField(MathHelper.class, "field_15722", double[].class);
+            removeField(MathHelper.class, "field_15725", float[].class);
+            removeField(MathHelper.class, "field_15723", int[].class);
+            removeField(MathHelper.class, "field_29852", long.class);
+            removeField(MathHelper.class, "field_29853", long.class);
+            removeField(MathHelper.class, "field_29854", long.class);
+            removeField(MathHelper.class, "field_29855", long.class);
+            removeField(MathHelper.class, "field_29844", float.class);
+            removeField(MathHelper.class, "field_29845", float.class);
+            removeField(MathHelper.class, "field_29846", float.class);
+            removeField(MathHelper.class, "field_29847", float.class);
+            removeField(MathHelper.class, "field_29848", float.class);
+            removeField(MathHelper.class, "field_29849", float.class);
+            removeField(MathHelper.class, "field_29856", float.class);
+            removeField(MathHelper.class, "field_46243", Vector3f.class);
+            removeField(MathHelper.class, "field_46244", Vector3f.class);
+            removeField(MathHelper.class, "field_29857", double.class);
+            removeField(MathHelper.class, "field_29858", int.class);
+            removeField(MathHelper.class, "field_29859", int.class);
+            removeField(MathHelper.class, "field_15728", double.class);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void removeArray(Class<?> clazz, String intermediaryFieldName, Class<?> arrayType) throws NoSuchFieldException, IllegalAccessException {
-        Field field = MappingTranslator.resolveField(clazz, intermediaryFieldName, arrayType);
+    private static void removeField(Class<?> clazz, String intermediaryFieldName, Class<?> fieldType) throws NoSuchFieldException, IllegalAccessException {
+        Field field = MappingTranslator.resolveField(clazz, intermediaryFieldName, fieldType);
         field.setAccessible(true);
 
         // Get Unsafe instance
